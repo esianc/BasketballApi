c51b9b6da526c08dc7c4830daf2f9e7866a93d4d
Erick Sian <64048988+esianc@users.noreply.github.com>
2025-08-26 00:08:49 -0600

    Primera carga

--- obj/Debug/net9.0/BasketballApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BasketballApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasketballApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e1c3f21191e5a461a6cac039a866cdd8c8eb194")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasketballApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasketballApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
